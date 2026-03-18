7099a63d3922ad081312f07d4db81bdf35d09b46
Dcstudios07 <Dcstudios0007@gmail.com>
2026-03-18 16:16:55 +0530

    fixed mongo uri and env setup

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ require('dotenv').config();
 app.use(cors()); 
 app.use(express.json()); 
 
-mongoose.connect('mongodb://localhost:27017/leadflow') .then(() => console.log("DB Connected")); 
+mongoose.connect('process.env.MONGO_URI') .then(() => console.log("DB Connected")); 
 
 app.use('/api/leads', leadRoutes); 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
